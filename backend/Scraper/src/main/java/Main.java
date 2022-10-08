26e0217cb640c5f659131aaa5ecae772f216a688
Max Sadler <maxsadler@Maxs-MacBook-Air.local>
2022-10-08 14:23:51 -0400

    Added ability to extract required player details - code refactore

--- backend/Scraper/src/main/java/Main.java
+++ backend/Scraper/src/main/java/Main.java
@@ -4,43 +4,94 @@ import java.util.List;
 import java.util.logging.Level;
 
 public class Main {
+    static WebClient webClient = null;
+
     public static void main(String[] args) {
 
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
-        final String URL = "https://www.nhl.com/player";
+        final String URL = "https://www.nhl.com/player/";
 
-        try(final WebClient webClient = new WebClient()) {
-
+        try {
+            webClient = new WebClient();
             webClient.getOptions().setThrowExceptionOnScriptError(false);
-            final HtmlPage page = webClient.getPage(URL);
 
-            webClient.waitForBackgroundJavaScript(10000);
-            HtmlTextInput input = page.getHtmlElementById("searchTerm");
-            input.setValue("Alex Ovechkin");
-            webClient.waitForBackgroundJavaScript(10000);
+            HtmlPage playerPage = getPlayerPage(URL, "Alex Ovechkin");
+            Player details = getPlayerDetails(playerPage);
 
-            List<HtmlAnchor> anchors = page.getAnchors();
-            HtmlPage playerPage = null;
-
-            for (HtmlAnchor anchor : anchors) {
-                if (anchor.getHrefAttribute().contains("alex-ovechkin")){
-                    playerPage = anchor.click();
-                    break;
-                }
-            }
-
-            if (playerPage != null){
-                webClient.waitForBackgroundJavaScript(5000);
-                System.out.println(playerPage.getTitleText());
-            }
+            System.out.println(details);
 
 
-
-        }catch(Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
+    }
+
+    public static HtmlPage getPlayerPage(String URL, String name) throws Exception {
+
+        final HtmlPage page = webClient.getPage(URL);
+        webClient.waitForBackgroundJavaScript(10000);
+
+        HtmlTextInput input = page.getHtmlElementById("searchTerm");
+        input.setValue(name);
+
+        webClient.waitForBackgroundJavaScript(10000);
+
+        List<HtmlAnchor> anchors = page.getAnchors();
+        HtmlPage playerPage = null;
+
+        for (HtmlAnchor anchor : anchors) {
+            if (anchor.getHrefAttribute().contains("alex-ovechkin")) {
+                playerPage = anchor.click();
+                break;
+            }
+        }
+
+        webClient.waitForBackgroundJavaScript(10000);
+
+        if (playerPage == null) {
+            throw new Exception("Failed to load player page");
+        }
+
+        return playerPage;
+    }
+
+    public static Player getPlayerDetails(HtmlPage page){
+        List<HtmlSpan> attributesWrapper = page.getByXPath("//div[@class='player-jumbotron-vitals__attributes']//span");
+        List<HtmlListItem> bioWrapper = page.getByXPath("//ul[@class='player-bio__list']//li");
 
 
+        String position = attributesWrapper.get(0).asNormalizedText();
+        String age = attributesWrapper.get(3).asNormalizedText();
+        String team = attributesWrapper.get(6).getTextContent();
+        String born = bioWrapper.get(2).asNormalizedText();
+        String shoots = bioWrapper.get(3).asNormalizedText();
+
+        return new Player(position, age.substring(4), team, born.substring(born.length() - 4), shoots.substring(7));
+    }
+
+    static class Player{
+        String position;
+        String age;
+        String team;
+        String born;
+        String shoots;
+
+        public Player(String position, String age, String team, String born, String shoots){
+            this.position = position;
+            this.age = age;
+            this.team = team;
+            this.born = born;
+            this.shoots = shoots;
+        }
+
+        @Override
+        public String toString(){
+            return "Position: " + this.position + "\n" +
+                    "Age: " + this.age + "\n" +
+                    "Team: " + this.team + "\n" +
+                    "Born: " + this.born + "\n" +
+                    "Shoots: " + this.shoots;
+        }
     }
 }
