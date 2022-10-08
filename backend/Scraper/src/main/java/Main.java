4560791c850260ce77e663c9cbbfcc759d4484f3
Max Sadler <maxsadler@Maxs-MacBook-Air.local>
2022-10-08 16:19:28 -0400

    Made iterative process for collecting player data

--- backend/Scraper/src/main/java/Main.java
+++ backend/Scraper/src/main/java/Main.java
@@ -1,6 +1,9 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
+
+import java.io.File;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 
 public class Main {
@@ -10,16 +13,25 @@ public class Main {
 
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
         final String URL = "https://www.nhl.com/player/";
+        String player = null;
 
         try {
             webClient = new WebClient();
             webClient.getOptions().setThrowExceptionOnScriptError(false);
 
-            HtmlPage playerPage = getPlayerPage(URL, "Alex Ovechkin");
-            Player details = getPlayerDetails(playerPage);
+            File file = new File("players.txt");
+            Scanner s = new Scanner(file);
 
-            System.out.println(details);
-
+            while (s.hasNext()) {
+                try {
+                    player = s.nextLine().strip();
+                    HtmlPage playerPage = getPlayerPage(URL, player);
+                    Player details = getPlayerDetails(playerPage);
+                    System.out.println(details);
+                } catch (Exception e) {
+                    System.out.println("Failed to fetch details for " + player);
+                }
+            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -41,7 +53,7 @@ public class Main {
         HtmlPage playerPage = null;
 
         for (HtmlAnchor anchor : anchors) {
-            if (anchor.getHrefAttribute().contains("alex-ovechkin")) {
+            if (anchor.getHrefAttribute().contains(String.join("-", name.split(" ")).toLowerCase())) {
                 playerPage = anchor.click();
                 break;
             }
@@ -56,7 +68,7 @@ public class Main {
         return playerPage;
     }
 
-    public static Player getPlayerDetails(HtmlPage page){
+    public static Player getPlayerDetails(HtmlPage page) throws IndexOutOfBoundsException {
         List<HtmlSpan> attributesWrapper = page.getByXPath("//div[@class='player-jumbotron-vitals__attributes']//span");
         List<HtmlListItem> bioWrapper = page.getByXPath("//ul[@class='player-bio__list']//li");
 
@@ -70,14 +82,14 @@ public class Main {
         return new Player(position, age.substring(4), team, born.substring(born.length() - 4), shoots.substring(7));
     }
 
-    static class Player{
+    static class Player {
         String position;
         String age;
         String team;
         String born;
         String shoots;
 
-        public Player(String position, String age, String team, String born, String shoots){
+        public Player(String position, String age, String team, String born, String shoots) {
             this.position = position;
             this.age = age;
             this.team = team;
@@ -86,7 +98,7 @@ public class Main {
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             return "Position: " + this.position + "\n" +
                     "Age: " + this.age + "\n" +
                     "Team: " + this.team + "\n" +
