22852c30775a0971f4ee024a05e1379e45e81c48
Max Sadler <maxsadler@Maxs-Air.ht.home>
2022-10-09 15:32:05 -0400

    Exception handling, JSON stringify to details file

--- backend/Scraper/src/main/java/Main.java
+++ backend/Scraper/src/main/java/Main.java
@@ -1,10 +1,19 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
-
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.google.gson.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
+
+// TODO: Think about adding threading to decrease runtime
 
 public class Main {
     static WebClient webClient = null;
@@ -13,7 +22,8 @@ public class Main {
 
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
         final String URL = "https://www.nhl.com/player/";
-        String player = null;
+        List<Player> players = new ArrayList<>();
+        List<String> failed = new ArrayList<>();
 
         try {
             webClient = new WebClient();
@@ -22,24 +32,32 @@ public class Main {
             File file = new File("players.txt");
             Scanner s = new Scanner(file);
 
+            String player = null;
+
             while (s.hasNext()) {
                 try {
                     player = s.nextLine().strip();
                     HtmlPage playerPage = getPlayerPage(URL, player);
-                    Player details = getPlayerDetails(playerPage);
-                    System.out.println(details);
-                } catch (Exception e) {
-                    System.out.println("Failed to fetch details for " + player);
+                    Player details = getPlayerDetails(playerPage, player);
+                    players.add(details);
+                } catch (FailingHttpStatusCodeException | MalformedURLException |
+                         ElementNotFoundException | IndexOutOfBoundsException |
+                         NullPointerException e) {
+                    System.out.println("Could not fetch details for " + player);
+                    failed.add(player);
                 }
             }
+
+            s.close();
+            writeToFile(players, failed);
+
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
 
-    public static HtmlPage getPlayerPage(String URL, String name) throws Exception {
+    public static HtmlPage getPlayerPage(String URL, String name) throws Exception{
 
         final HtmlPage page = webClient.getPage(URL);
         webClient.waitForBackgroundJavaScript(10000);
@@ -61,14 +79,12 @@ public class Main {
 
         webClient.waitForBackgroundJavaScript(10000);
 
-        if (playerPage == null) {
-            throw new Exception("Failed to load player page");
-        }
+        if (playerPage == null) { throw new NullPointerException(); }
 
         return playerPage;
     }
 
-    public static Player getPlayerDetails(HtmlPage page) throws IndexOutOfBoundsException {
+    public static Player getPlayerDetails(HtmlPage page, String name) throws IndexOutOfBoundsException {
         List<HtmlSpan> attributesWrapper = page.getByXPath("//div[@class='player-jumbotron-vitals__attributes']//span");
         List<HtmlListItem> bioWrapper = page.getByXPath("//ul[@class='player-bio__list']//li");
 
@@ -79,17 +95,27 @@ public class Main {
         String born = bioWrapper.get(2).asNormalizedText();
         String shoots = bioWrapper.get(3).asNormalizedText();
 
-        return new Player(position, age.substring(4), team, born.substring(born.length() - 4), shoots.substring(7));
+        return new Player(name, position, age.substring(4), team, born.substring(born.length() - 4), shoots.substring(7));
+    }
+
+    public static void writeToFile(List<Player> players, List<String> failed) throws IOException {
+        Gson gson = new Gson();
+        String playerJSON = gson.toJson(players);
+        Files.writeString(Path.of("details.json"), playerJSON);
+        Files.writeString(Path.of("failed.txt"), failed.toString());
     }
 
     static class Player {
+
+        String name;
         String position;
         String age;
         String team;
         String born;
         String shoots;
 
-        public Player(String position, String age, String team, String born, String shoots) {
+        public Player(String name, String position, String age, String team, String born, String shoots) {
+            this.name = name;
             this.position = position;
             this.age = age;
             this.team = team;
@@ -99,11 +125,12 @@ public class Main {
 
         @Override
         public String toString() {
-            return "Position: " + this.position + "\n" +
+            return  "Name: " + this.name + "\n" +
+                    "Position: " + this.position + "\n" +
                     "Age: " + this.age + "\n" +
                     "Team: " + this.team + "\n" +
                     "Born: " + this.born + "\n" +
-                    "Shoots: " + this.shoots;
+                    "Shoots: " + this.shoots + "\n";
         }
     }
 }
