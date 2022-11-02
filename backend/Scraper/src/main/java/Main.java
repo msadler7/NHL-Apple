8c1a2f6837d1a90b75c3ba6a147c75df6ac3fd0e
Max Sadler <maxsadler@Maxs-Air.ht.home>
2022-11-01 22:35:27 -0400

    refactor to support threading

--- backend/Scraper/src/main/java/Main.java
+++ backend/Scraper/src/main/java/Main.java
@@ -1,98 +1,111 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.google.gson.*;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
-// TODO: Think about adding threading to decrease runtime
-
 public class Main {
-    static WebClient webClient = null;
+    final static String URL = "https://www.nhl.com/player/";
+    final static List<Player> players = Collections.synchronizedList(new ArrayList<>());
+    final static List<String> failed = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) {
 
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
-        final String URL = "https://www.nhl.com/player/";
-
-        List<Player> players = new ArrayList<>();
-        List<String> failed = new ArrayList<>();
 
         try {
             String[] names = Files.readString(Path.of("players.txt"), StandardCharsets.UTF_8).split("\n");
+            ExecutorService service = Executors.newFixedThreadPool(5);
 
             for (String name : names) {
-                try {
-                    webClient = new WebClient();
-                    webClient.getOptions().setCssEnabled(false);
-                    webClient.getOptions().setPopupBlockerEnabled(true);
-                    webClient.getOptions().setThrowExceptionOnScriptError(false);
-
-                    HtmlPage playerPage = getPlayerPage(URL, name);
-                    Player details = getPlayerDetails(playerPage, name);
-                    players.add(details);
-                } catch (FailingHttpStatusCodeException | MalformedURLException |
-                         ElementNotFoundException | IndexOutOfBoundsException |
-                         NullPointerException e) {
-                    System.out.println("Could not fetch details for " + name);
-                    failed.add(name);
-                }
+                service.execute(new Task(name));
             }
+            service.shutdown();
+            service.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
             writeToFile(players, failed);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static HtmlPage getPlayerPage(String URL, String name) throws Exception {
+    static class Task implements Runnable {
+        String name;
+        WebClient webClient;
 
-        final HtmlPage page = webClient.getPage(URL);
-        webClient.waitForBackgroundJavaScript(15000);
+        public Task(String name) {
+            this.name = name;
+            this.webClient = new WebClient();
+        }
 
-        HtmlTextInput input = page.getHtmlElementById("searchTerm");
-        input.setValue(name);
+        @Override
+        public void run() {
 
-        webClient.waitForBackgroundJavaScript(15000);
+            try {
+                webClient.getOptions().setCssEnabled(false);
+                webClient.getOptions().setPopupBlockerEnabled(true);
+                webClient.getOptions().setThrowExceptionOnScriptError(false);
 
-        List<HtmlAnchor> anchors = page.getByXPath("//td[@class='results-table__player-td']//a[@class='name-link']");
-        HtmlPage playerPage = null;
-        String href = String.join("-", name.split(" ")).toLowerCase();
-
-        System.out.println("Finding anchor.....");
-        for (HtmlAnchor anchor : anchors) {
-            if (anchor.getHrefAttribute().contains(href)) {
-                playerPage = anchor.click();
-                break;
+                HtmlPage playerPage = getPlayerPage(URL);
+                Player details = getPlayerDetails(playerPage);
+                System.out.println("Found " + name);
+                players.add(details);
+            } catch (Exception e) {
+                System.out.println("Could not fetch details for " + name);
+                failed.add(name);
             }
         }
 
-        webClient.waitForBackgroundJavaScript(15000);
-        if (playerPage == null) {
-            throw new NullPointerException();
+        public HtmlPage getPlayerPage(String URL) throws Exception {
+
+            final HtmlPage page = webClient.getPage(URL);
+            webClient.waitForBackgroundJavaScript(35000);
+
+            HtmlTextInput input = page.getHtmlElementById("searchTerm");
+            input.setValue(name);
+
+            webClient.waitForBackgroundJavaScript(35000);
+
+            List<HtmlAnchor> anchors = page.getByXPath("//td[@class='results-table__player-td']//a[@class='name-link']");
+            HtmlPage playerPage = null;
+            String href = name.replace(" ", "-").toLowerCase();
+
+            for (HtmlAnchor anchor : anchors) {
+                if (anchor.getHrefAttribute().contains(href)) {
+                    playerPage = anchor.click();
+                    break;
+                }
+            }
+
+            webClient.waitForBackgroundJavaScript(15000);
+            if (playerPage == null) {
+                throw new NullPointerException();
+            }
+
+            return playerPage;
         }
 
-        return playerPage;
-    }
-
-    public static Player getPlayerDetails(HtmlPage page, String name) throws IndexOutOfBoundsException {
-        List<HtmlSpan> attributesWrapper = page.getByXPath("//div[@class='player-jumbotron-vitals__attributes']//span");
-        List<HtmlListItem> bioWrapper = page.getByXPath("//ul[@class='player-bio__list']//li");
+        public Player getPlayerDetails(HtmlPage page) throws IndexOutOfBoundsException {
+            List<HtmlSpan> attributesWrapper = page.getByXPath("//div[@class='player-jumbotron-vitals__attributes']//span");
+            List<HtmlListItem> bioWrapper = page.getByXPath("//ul[@class='player-bio__list']//li");
 
 
-        String position = attributesWrapper.get(0).asNormalizedText();
-        String age = attributesWrapper.get(3).asNormalizedText();
-        String team = attributesWrapper.get(6).getTextContent();
-        String born = bioWrapper.get(2).asNormalizedText();
-        String shoots = bioWrapper.get(3).asNormalizedText();
+            String position = attributesWrapper.get(0).asNormalizedText();
+            String age = attributesWrapper.get(3).asNormalizedText();
+            String team = attributesWrapper.get(6).getTextContent();
+            String born = bioWrapper.get(2).asNormalizedText();
+            String shoots = bioWrapper.get(3).asNormalizedText();
 
-        return new Player(name, position, age.substring(4), team, born.substring(born.length() - 4), shoots.substring(7));
+            return new Player(name, position, age.substring(4), team, born.substring(born.length() - 4), shoots.substring(7));
+        }
     }
 
     public static void writeToFile(List<Player> players, List<String> failed) throws IOException {
